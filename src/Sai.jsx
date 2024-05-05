f6daa9bc8a13d9c25afe76e1006fdba0a3cbe5e7
saifulannex <saifulannex@gmail.com>
2024-05-05 17:48:20 +0600

    first commit

--- src/Sai.jsx
+++ src/Sai.jsx
@@ -31,9 +31,9 @@ const Sai = () => {
             <div className="hero-content flex-col lg:flex-row-reverse items-center">
                 <img src={saifulImgurLink} alt="Race Family" className={`rounded-lg shadow-2xl mb-4 lg:mb-0 lg:mr-8 mx-auto w-${width} h-${height}`} />
                 <div className="text-center lg:text-left ml-4 mr-20">
-                    <h1 className="text-5xl text-fuchsia-600 font-bold">I am <br /> Saiful Islam.</h1>
-                    <h1 className="text-2xl text-neutral-950 font-bold">CEO <br /> Harbour IT </h1>
-                    <p className="py-6">
+                    <h1 className="text-4xl md:text-5xl lg:text-6xl text-fuchsia-600 font-bold leading-tight">I am <br /> Saiful Islam.</h1>
+                    <h1 className="text-lg md:text-2xl lg:text-3xl text-neutral-950 font-bold mt-4">CEO <br /> Harbour IT </h1>
+                    <p className="py-4 text-sm md:text-base">
                         I have been working with digital marketing for 5 years.
                     </p>
                     <a href={facebookURL} target="_blank" rel="noopener noreferrer" className="btn btn-primary">Facebook</a>
