01154d057ecbe00c7d5131e43c6d84e4399f70e7
saifulannex <saifulannex@gmail.com>
2024-05-05 17:27:02 +0600

    first commit

--- src/Sai.jsx
+++ src/Sai.jsx
@@ -5,7 +5,6 @@ const Sai = () => {
     return (
         <div className="hero min-h-screen bg-base-200">
             <div className="hero-content flex-col lg:flex-row-reverse items-center">
-                <img src={saifulImgurLink} alt="Race Family" className="max-w-sm rounded-lg shadow-2xl mb-4 lg:mb-0 lg:mr-8 mx-auto" />
                 <div className="text-center lg:text-left ml-4 mr-20">
                     <h1 className="text-5xl text-fuchsia-600 font-bold">I am <br /> Saiful Islam.</h1>
                     <h1 className="text-2xl text-neutral-950 font-bold">CEO <br /> Harbour IT </h1>
@@ -14,6 +13,7 @@ const Sai = () => {
                     </p>
                     <a href={facebookURL} target="_blank" rel="noopener noreferrer" className="btn btn-primary">Facebook</a>
                 </div>
+                <img src={saifulImgurLink} alt="Race Family" className="max-w-sm rounded-lg shadow-2xl mb-4 lg:mb-0 lg:mr-8 mx-auto" />
             </div>
         </div>
     );
