3c5d5282eb93ceda99f1fb2968a9c4d6c905e701
saifulannex <saifulannex@gmail.com>
2024-05-05 17:22:29 +0600

    first commit

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,24 +1,20 @@
-import FacebookIcon from './assets/Facebook.png';
+import React from 'react';
+import FacebookIcon from './assets/Facebook_Logo.png';
 import YoutubeIcon from './assets/Youtube.png';
 import InstagramIcon from './assets/Group.png';
 
 const Footer = () => {
-  const facebookLink = "https://www.facebook.com/profile.php?id=61559198431989"; // আপনার ফেসবুক পেজের লিংক
-  const youtubeLink = "https://www.youtube.com/channel/UC3JrcsUmJj5sGkszbpYA8ww"; // ইউটিউব চ্যানেলের লিংক
-  const Facebookgroup = "https://www.facebook.com/groups/racefamilybd"; // ইনস্টাগ্রাম পেজের লিংক
-
   return (
-    <footer className="footer p-10 bg-purple-950 text-neutral-content relative">
-      <img src="https://imgur.com/FZH6ECa.png" alt="ACME Industries Ltd." className="w-12 h-12" />
-      <aside className="text-center lg:text-left">
-        <p>Race Family<br/>01869-296343</p>
+    <footer className="footer p-10 bg-neutral text-neutral-content">
+      <aside>
+        <p>Race Family<br/>01869-296343 <br /> racefamilybd@gmail.com </p>
       </aside> 
-      <nav className="text-center">
-        <h6 className="footer-title">Social</h6> 
-        <div className="grid grid-flow-col gap-4 justify-center">
-          <a href={facebookLink} target="_blank" rel="noopener noreferrer"><img src={FacebookIcon} alt="Facebook" className="w-6 h-6" /></a>
-          <a href={youtubeLink} target="_blank" rel="noopener noreferrer"><img src={YoutubeIcon} alt="Youtube" className="w-6 h-6" /></a>
-          <a href={Facebookgroup} target="_blank" rel="noopener noreferrer"><img src={InstagramIcon} alt="Instagram" className="w-6 h-6" /></a>
+      <nav>
+        <h6 className="footer-title">Social link</h6> 
+        <div className="grid grid-flow-col gap-4">
+          <a href="https://www.facebook.com/bdracefamily" target="_blank" rel="noopener noreferrer"><img src={FacebookIcon} alt="Facebook" className="w-6 h-6" /></a>
+          <a href="https://www.youtube.com/channel/UCSpg5S0UnoiCn8iVYEHtW3Q" target="_blank" rel="noopener noreferrer"><img src={YoutubeIcon} alt="Youtube" className="w-6 h-6" /></a>
+          <a href="https://www.facebook.com/groups/racefamilybd" target="_blank" rel="noopener noreferrer"><img src={InstagramIcon} alt="Instagram" className="w-6 h-6" /></a>
         </div>
       </nav>
     </footer>
--- src/HNavBar.jsx
+++ src/HNavBar.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import Logo from './assets/Logo.png';
-import FacebookIcon from './assets/Facebook.png';
+import FacebookIcon from './assets/Facebook_Logo.png';
 import YoutubeIcon from './assets/Youtube.png';
 import InstagramIcon from './assets/Group.png';
 
@@ -10,7 +9,6 @@ const NavBar = () => {
         <div className="navbar bg-purple-950 text-white flex flex-col lg:flex-row justify-between items-center px-4 py-2">
             <div className="flex items-center">
                 <a href="https://www.facebook.com/profile.php?id=61559198431989" target="_blank" rel="noopener noreferrer" className="mx-2">
-                    <img src={Logo} alt="Race Family" className="w-6 h-6" />
                 </a>
                 <div className="text-center lg:text-left">
                     <h1 className='text-3xl'>Race Family</h1>
@@ -32,10 +30,10 @@ const NavBar = () => {
                 </ul>
             </div>
             <div className="social-icons flex items-center">
-                <a href="https://www.facebook.com/profile.php?id=61559198431989" target="_blank" rel="noopener noreferrer" className="mx-2">
+                <a href="https://www.facebook.com/bdracefamily" target="_blank" rel="noopener noreferrer" className="mx-2">
                     <img src={FacebookIcon} alt="Facebook" className="w-6 h-6" />
                 </a> 
-                <a href="https://www.youtube.com/channel/UC3JrcsUmJj5sGkszbpYA8ww" target="_blank" rel="noopener noreferrer" className="mx-2">
+                <a href="https://www.youtube.com/channel/UCSpg5S0UnoiCn8iVYEHtW3Q" target="_blank" rel="noopener noreferrer" className="mx-2">
                     <img src={YoutubeIcon} alt="Youtube" className="w-6 h-6" />
                 </a>
                 <a href="https://www.facebook.com/groups/racefamilybd" target="_blank" rel="noopener noreferrer" className="mx-2">
